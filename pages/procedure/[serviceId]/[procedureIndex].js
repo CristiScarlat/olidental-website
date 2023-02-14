d71035efb622b32ef8c4709980b90ac65f00b4e7
scarlat-trainer <78.cristi@gmail.com>
2023-02-14 15:50:11 +0200

    add before/mid/after image carousel

--- pages/procedure/[serviceId]/[procedureIndex].js
+++ pages/procedure/[serviceId]/[procedureIndex].js
@@ -1,60 +1,57 @@
-import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import { services } from "../../../utils/uiConstants";
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { services } from '../../../utils/uiConstants';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
 const Procedure = () => {
-    const router = useRouter();
-    const { serviceId, procedureIndex } = router.query;
+  const [imagesIndex, setImagesIndex] = useState(0);
+  const router = useRouter();
+  const { serviceId, procedureIndex } = router.query;
 
-    return (
-        <div className="services-container m-auto">
-            <div className="pt-4 pb-4 bg-gray">
-                <div className="m-auto" style={{ maxWidth: '60rem' }}>
-                    <div className="text-center">
-                        <img src={services[serviceId]?.procedures[procedureIndex]?.logo || ''} alt="..." style={{ width: "75px" }} />
-                        <h3>{services[serviceId]?.procedures[procedureIndex]?.title || ''}</h3>
-                    </div>
-                </div>
-                <div className="dark-gray-bg pt-3 mb-2 d-flex justify-content-center custom-scroll" style={{ overflowX: 'auto' }}>
-                    {services[serviceId]?.procedures[procedureIndex]?.cazuri?.images.map((arr, index) => (
-                        <div className="mb-3" key={"before-after" + index}>
-                            <Carousel
-                                className="carousel-custom-root"
-                                stopOnHover={false}
-                                showThumbs={false}
-                                showArrows={false}
-                                showIndicators={false}
-                                dynamicHeight={true}
-                                infiniteLoop
-                                autoPlay={true}
-                                showStatus={false}
-                                interval={2000}
-                                preventMovementUntilSwipeScrollTolerance={true}
-                                swipeScrollTolerance={50}
-                                animationHandler="fade"
-                                swipeable={false}
-                            >
-                                {arr.map((image, index) => <div key={image + index}>
-                                    <img src={`/images/${services[serviceId]?.procedures[procedureIndex]?.cazuri?.dirPath}/thumbnail_${image}`} style={{ width: 240 }} />
-                                </div>)}
-                            </Carousel>
-                            {/* {arr.map(image => <div>
-                            <img src={`/images/${services[serviceId]?.procedures[procedureIndex]?.cazuri?.dirPath}/${image}`} style={{ width: 240 }} />
-                        </div>)} */}
-                        </div>
-                    ))}
-                </div>
-                <div className="m-auto ps-4 pe-4" style={{ maxWidth: '60rem' }}>
-                    <div className="services-container-description" dangerouslySetInnerHTML={{ __html: services[serviceId]?.procedures[procedureIndex]?.description || '' }}></div>
-                </div>
-            </div>
-            {/* <div className="d-flex" style={{ overflowX: 'auto' }}>
+  useEffect(() => {
+    const id = setInterval(() => {
+      handleChangeImages();
+    }, 5000);
+    return () => clearInterval(id);
+  });
+  const handleChangeImages = () => {
+    setImagesIndex((state) => {
+      if (state === 5) return 0;
+      else return state + 1;
+    });
+  };
+
+  return (
+    <div className="services-container m-auto">
+      <div className="pt-4 pb-4 bg-gray">
+        <div className="m-auto" style={{ maxWidth: '60rem' }}>
+          <div className="text-center">
+            <img src={services[serviceId]?.procedures[procedureIndex]?.logo || ''} alt="..." style={{ width: '75px' }} />
+            <h3>{services[serviceId]?.procedures[procedureIndex]?.title || ''}</h3>
+          </div>
+        </div>
+        <div className="pt-3 mb-2 d-flex flex-column custom-scroll" style={{ overflowX: 'auto' }}>
+          <div className="mb-3 d-flex me-auto ms-auto gap-md-1" style={{ minHeight: '12rem' }} onClick={handleChangeImages}>
+            {services[serviceId]?.procedures[procedureIndex]?.cazuri?.images[imagesIndex].map((image, index) => (
+              <div key={image + index}>
+                <img src={`/images/${services[serviceId]?.procedures[procedureIndex]?.cazuri?.dirPath}/thumbnail_${image}`} className="procedure-cazuri-img" />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="m-auto ps-4 pe-4" style={{ maxWidth: '60rem' }}>
+          <div
+            className="services-container-description"
+            dangerouslySetInnerHTML={{ __html: services[serviceId]?.procedures[procedureIndex]?.description || '' }}
+          ></div>
+        </div>
+      </div>
+      {/* <div className="d-flex" style={{ overflowX: 'auto' }}>
                 {services[serviceId]?.procedures[procedureIndex]?.images?.map(image => <img key={image} src={`/images/services/${services[serviceId]?.procedures[procedureIndex]?.dirPath}/${image}`} style={{ width: 280 }} />)}
             </div> */}
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Procedure;
