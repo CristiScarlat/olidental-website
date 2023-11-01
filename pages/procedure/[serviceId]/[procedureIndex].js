9cdd86c7940ad4b5ca90f4e46418ea2fb872be65
scarlat-trainer <78.cristi@gmail.com>
2023-11-01 15:53:13 +0200

    add indication for three images comparator for mobile in RO language

--- pages/procedure/[serviceId]/[procedureIndex].js
+++ pages/procedure/[serviceId]/[procedureIndex].js
@@ -119,7 +119,7 @@ const Procedure = () => {
             <span>{`${indexCaz + 1}/${cazuri?.length}`}</span>
             <button onClick={handleNavNext}><BiCaretRight /></button>
           </div>
-          {(cazuri?.length && cazuri[indexCaz].images[imgNo].length > 2) && <p className='text-center opacity-75 visible-400'>Touch image to see full width.</p>}
+          {(cazuri?.length && cazuri[indexCaz].images[imgNo].length > 2) && <p className='text-center opacity-75 visible-400'>Atinge imaginea pentru a o vedea în întregime.</p>}
         </div>
         <div className='m-auto p-2' style={{ maxWidth: '60rem' }}>
           <div
