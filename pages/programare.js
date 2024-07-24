4edeaf9ff001c263b67a038101e82afd55be7383
scarlat-trainer <78.cristi@gmail.com>
2024-07-24 10:18:13 +0300

    add emailJS and spinner and toast messages for emailjs success or fail

--- pages/programare.js
+++ pages/programare.js
@@ -1,13 +1,17 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Spinner from 'react-bootstrap/Spinner';
 import ReCAPTCHA from "react-google-recaptcha";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import emailjs from '@emailjs/browser';
 
 const Programare = () => {
   const [recaptcha, setRecaptcha] = useState();
+  const [spinner, setSpinner] = useState(false);
 
+  const formRef = useRef();
   const isFormValid = (name, surname, phone, email, interval, gdpr) => {
     let valid = true;
     if(recaptcha === undefined) {
@@ -46,7 +50,29 @@ const Programare = () => {
     const gdpr = e.target[5].checked;
 
     if(isFormValid(name, surname, phone, email, interval, gdpr)){
-      console.log("send email")
+      setSpinner(true)
+      const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+      const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+      emailjs
+        .sendForm(serviceId, templateId, e.target, {
+          publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
+        })
+        .then(
+          () => {
+            setSpinner(false);
+            toast.success(`Un email de confirmare a fost trimis la ${email}, mulțumim.`);
+          },
+          (error) => {
+            setSpinner(false);
+            console.log('FAILED...', error.text);
+            toast.error(`Ne pare rău, emailul de confirmare pe adresa ${email} nu a putut fi trimis. Verificați din nou adresa de email completată in formular sau încercați mai târziu.`)
+          },
+        )
+        .catch(error => {
+          setSpinner(false);
+          console.log('FAILED...', error);
+          toast.error(`Ne pare rău, emailul de confirmare pe adresa ${email} nu a putut fi trimis. Verificați din nou adresa de email completată in formular sau încercați mai târziu.`)
+        })
     }
 
   }
@@ -58,32 +84,32 @@ const Programare = () => {
   return (
     <div>
       <ToastContainer newestOnTop theme="colored" position={"bottom-right"}/>
-      <Form className="m-auto p-3 bg-gray rounded" style={{maxWidth: "50rem"}} id="form-programare" onSubmit={handleSubmit}>
+      <Form ref={formRef} className="m-auto p-3 bg-gray rounded" style={{maxWidth: "50rem"}} id="form-programare" onSubmit={handleSubmit}>
         <Form.Group className='mb-3' controlId='formName'>
           <Form.Label>Nume</Form.Label>
-          <Form.Control type='text' placeholder='Nume' />
+          <Form.Control type='text' placeholder='Nume' name='user_name' autoComplete="off"/>
         </Form.Group>
-        <Form.Group className='mb-3' controlId='formSurname'>
+        <Form.Group className='mb-3' controlId='formSurname' >
           <Form.Label>Prenume</Form.Label>
-          <Form.Control type='text' placeholder='Prenume' />
+          <Form.Control type='text' placeholder='Prenume' name='user_surname' autoComplete="off"/>
         </Form.Group>
         <Form.Group className='mb-3' controlId='formPhone'>
           <Form.Label>Telefon</Form.Label>
-          <Form.Control type='text' placeholder='Telefon' />
+          <Form.Control type='text' placeholder='Telefon' name='user_phone' autoComplete="off"/>
         </Form.Group>
         <Form.Group className='mb-3' controlId='formEmail'>
           <Form.Label>Email address</Form.Label>
-          <Form.Control type='email' placeholder='Adresa email' />
+          <Form.Control type='email' placeholder='Adresa email' name='user_email' autoComplete="off"/>
           <Form.Text className='text-muted'>
             Confirmarea programării va fi trimisă pe această adresă de email.
           </Form.Text>
         </Form.Group>
         <Form.Group className='mb-3' >
           <Form.Label>Intervalul de timp preferat</Form.Label>
-          <Form.Select>
-            <option value="null">oricând</option>
-            <option value="morning">înainte de ora 12</option>
-            <option value="afternoon">după ora 12</option>
+          <Form.Select name='user_time_interval'>
+            <option value="oricând">oricând</option>
+            <option value="înainte de ora 12">înainte de ora 12</option>
+            <option value="după ora 12">după ora 12</option>
           </Form.Select>
         </Form.Group>
         <Form.Group className='mb-3' controlId='formBasicCheckbox' id="customCheckbox">
@@ -94,8 +120,11 @@ const Programare = () => {
           onChange={onRecaptchaChange}
         />
         <div className="d-flex justify-content-end">
-          <Button variant={'success'} type='submit'>
-            Fă-ți o programare
+          <Button variant={'success'} type='submit' className="d-flex align-items-center gap-2">
+            <span>Fă-ți o programare</span>
+            {spinner && <Spinner animation="border" variant="light" role="status" size="sm">
+              <span className="visually-hidden">Se trimite...</span>
+            </Spinner>}
           </Button>
         </div>
       </Form>
