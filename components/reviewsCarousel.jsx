c7b30987f339a290ba5b274b1aa4489b8afb65fd
Cristi Scarlat <78.cristi@gmail.com>
2026-04-11 12:13:45 +0300

    remove title from review section

--- components/reviewsCarousel.jsx
+++ components/reviewsCarousel.jsx
@@ -5,7 +5,7 @@ import Script from 'next/script'
 export default function GoogleReviews() {
   return (
     <section className="py-10 bg-gray" style={{borderBottom: "3px solid #00000014"}}>
-      <h2 className="text-2xl font-bold text-center mb-6">Testimoniale</h2>
+      {/* <h2 className="font-bold text-center mb-6 text-white">Testimoniale</h2> */}
 
       {/* PASTE THE DIV HERE */}
       <div id="JFWebsiteWidget-019d7bc2c0b3747ebcdd33f78f7d1c561170"></div>
